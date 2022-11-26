41d22d5baa6a41ad9a1ad32d571ae91b1fdf10bf
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-26 19:32:41 +0600

    RegExp

--- basics.js
+++
@@ -1,5 +0,0 @@
-let nam = "Shourov";
-
-const check = (data) => data === nam;
-
-console.log(check(""));
---
+++ basics.ts
@@ -0,0 +1,7 @@
+let nam: string = "Shourov";
+
+let check: boolean = true;
+
+let myRegEx: RegExp = /foo/;
+
+console.log();
